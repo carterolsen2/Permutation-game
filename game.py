ccc71e23e4a7f3582debc620746503652247ae7d
Carter Olsen <carterolsen2@gmail.com>
2014-05-14 13:27:53 -0600

    Added save/load option, and menu for user

--- game.py
+++ game.py
@@ -44,7 +44,13 @@ def find_words(val, n=None):
     return results
     # for w in sorted(results):
     #     print w
-
+def permutation_game():
+    val = raw_input("Enter a word: ")
+    results = find_words(val)
+    for w in sorted(results):
+        if len(w) > 3:
+            print w
+    print
 
 # val = raw_input('Enter a word: ')
 # find_words(val)
@@ -57,7 +63,6 @@ def find_words(val, n=None):
 # many words of a given length remain.
 
 
-
 def shuffle_word(word):
     from random import shuffle
 
@@ -66,26 +71,29 @@ def shuffle_word(word):
     return ''.join(word)
 
 
-def scrambled_words():
+def scrambled_words(shuffled_word=None, len4_found=None, len5_found=None,
+                    len6_found=None):
+
     word_list = list(get_word_set(6))
 
-    from random import choice
+    if shuffled_word is None:
+        from random import choice
+        selected_word = choice(word_list)
+        shuffled_word = shuffle_word(selected_word)
+        len6_found = []
+        len5_found = []
+        len4_found = []
 
-    selected_word = choice(word_list)
-    # print selected_word
-    find_words(selected_word, 4)
-    len6 = list(find_words(selected_word, 6))
-    len5 = list(find_words(selected_word, 5))
-    len4 = list(find_words(selected_word, 4))
-    shuffled_word = shuffle_word(selected_word) + ':'
+    len6 = list(find_words(shuffled_word, 6))
+    len5 = list(find_words(shuffled_word, 5))
+    len4 = list(find_words(shuffled_word, 4))
 
-    len6_found = []
-    len5_found = []
-    len4_found = []
+    print "Enter 's' to save and quit to the menu."
+    print "Enter 'q' to quit to the menu without saving."
+
     while len6 or len5 or len4:
         print
-        print shuffled_word
-
+        print shuffled_word + ': '
         print "\nFound six letter words: " + str(len6_found)
         print str(len(len6)) + " six letter words left."
         print "Found five letter words: " + str(len5_found)
@@ -94,6 +102,24 @@ def scrambled_words():
         print str(len(len4)) + " four letter words left."
 
         user_choice = raw_input("\nEnter a word: ")
+
+        if user_choice == 'q':
+            quit()
+
+        if user_choice == 's':
+            save_game_name = raw_input("\nEnter a name for the saved game: ")
+            with open('saved_games/' + save_game_name + '.txt', 'w') as f:
+                f.write(save_game_name + '\n')
+                f.write(shuffled_word + '\n')
+                f.write(str(len4_found) + '\n')
+                f.write(str(len5_found) + '\n')
+                f.write(str(len6_found) + '\n')
+                f.write('\n')
+            print
+            return
+
+        if user_choice in len6 or user_choice in len5 or user_choice in len4:
+            print "Found word!"
 
         if user_choice in len6:
             len6_found.append(user_choice)
@@ -107,4 +133,54 @@ def scrambled_words():
         else:
             print "Not a valid choice."
     print "You won the game!"
-scrambled_words()
+
+def load_game():
+    import os
+    game_saves = [f for f in os.listdir('saved_games')
+                  if f.lower().endswith('.txt')]
+
+    print "\nSaved games: "
+    for save in game_saves:
+        print "\t" + save[:-4]
+
+    user_choice = raw_input("\nType the name of a game save: ")
+
+    with open('saved_games/' + user_choice + '.txt') as f:
+        import ast
+        f.readline()
+        shuffled_word = f.readline().strip()
+        len4_found = ast.literal_eval(f.readline())
+        len5_found = ast.literal_eval(f.readline())
+        len6_found = ast.literal_eval(f.readline())
+
+    scrambled_words(shuffled_word, len4_found, len5_found, len6_found)
+
+
+def main():
+    while True:
+        print "Which game do you want to play?"
+        print "1. Word guessing game."
+        print "2. Word guessing game solver."
+        user_choice = raw_input("> ")
+
+        if user_choice == '1':
+            print("Enter a number to choose an option: ")
+            print ("1: start a new game")
+            print ("2: load a saved game")
+
+            user_choice = raw_input('> ')
+
+            if user_choice == '1':
+                scrambled_words()
+            elif user_choice == '2':
+                load_game()
+            else:
+                print"Not a valid option."
+                return
+        elif user_choice == '2':
+            permutation_game()
+        else:
+            print "Not a valid choice. Please print a valid choice."
+
+
+main()
